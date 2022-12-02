05eeab7c9c1bf3b8e61af0b724c7883fcc776457
Alex Kelly <kellya@arachnitech.com>
2022-12-02 11:20:30 -0500

    add abuseipdb and crowdsec cti links for IP

--- matrix_webhook/formatters/crowdsec.py
+++ matrix_webhook/formatters/crowdsec.py
@@ -1,11 +1,16 @@
 def formatter(data, headers):
-    """ format a message sent with crowdsec http endpoints"""
+    """format a message sent with crowdsec http endpoints"""
     data_out = ""
     for row in data["body"]:
-       if "crowdsecurity" in row['scenario']:
-          source, scenario, *_ = row['scenario'].split('/')
-          row['scenario'] = f"[{scenario}](https://hub.crowdsec.net/author/crowdsecurity/configurations/{scenario})"
-       data_out += f"{row['host']} has been banned {row['duration']} due to {row['scenario']}\n\n[AbuseIPDB](https://www.abuseipdb.com/check{row['host'])" 
+        if "crowdsecurity" in row["scenario"]:
+            source, scenario, *_ = row["scenario"].split("/")
+            row[
+                "scenario"
+            ] = f"[{scenario}](https://hub.crowdsec.net/author/crowdsecurity/configurations/{scenario})"
+        data_out += (
+            f"{row['host']} has been banned {row['duration']} due to {row['scenario']}\n\n"
+            f"[AbuseIPDB](https://www.abuseipdb.com/check/{row['host']})|"
+            f"[Crowdsec](https://app.crowdsec.net/cti/{row['host']})\n\n"
+        )
     data["body"] = data_out
     return data
-
